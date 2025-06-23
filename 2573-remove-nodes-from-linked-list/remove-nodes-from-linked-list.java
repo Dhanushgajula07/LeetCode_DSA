784a81d94a8aea02f2bba44fbfff535c4802bc3d
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-06-23 18:10:39 +0530

    Time: 6 ms (91.45%) | Memory: 69.7 MB (22.14%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -7,34 +7,73 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+//  */
+// class Solution {
+//     public ListNode removeNodes(ListNode head) {
+//         // my 1st thought was to implemet monoyonic satck and remove if st.ppek() <= curr
+//         // but then i should convert to linked list 
+
+//         if(head == null) return null;
+//         // lets try 
+//         Stack<Integer> st = new Stack<>();
+//         while(head != null){
+//             int val = head.val;
+//             while(!st.isEmpty() && st.peek() < val){
+//                 st.pop();
+//             }
+//             st.push(val);
+//             head = head.next;
+//         }
+//         ListNode dummy = new ListNode(-1);
+//         ListNode node = dummy;
+
+//         for(Integer it : st){
+//             ListNode curr = new ListNode(it);
+//             node.next = curr;
+//             node = node.next;
+//         }
+//         return dummy.next;
+
+//         // tc = O(n) + O(n)
+//         // sc = O(n) + O(n) dummy node
+//     }
+// }
+
+
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        // my 1st thought was to implemet monoyonic satck and remove if st.ppek() <= curr
-        // but then i should convert to linked list 
-
+        // as of neetcccode.io 
+        // we can use reverse method to reverse and apply logic and reverse back
         if(head == null) return null;
-        // lets try 
-        Stack<Integer> st = new Stack<>();
-        while(head != null){
-            int val = head.val;
-            while(!st.isEmpty() && st.peek() < val){
-                st.pop();
+        ListNode temp = reverse(head);
+        // logic to remove
+        ListNode curr = temp;
+        int curr_max = temp.val;
+        // we start from last but second because for fore we dont remove  the last one
+        // temp.next is the element to be removed
+        while(curr.next != null){
+            if(curr.next.val < curr_max){
+                curr.next = curr.next.next;
             }
-            st.push(val);
-            head = head.next;
+            else{
+                curr_max = curr.next.val;
+                curr = curr.next;
+            }
         }
-        ListNode dummy = new ListNode(-1);
-        ListNode node = dummy;
-
-        for(Integer it : st){
-            ListNode curr = new ListNode(it);
-            node.next = curr;
-            node = node.next;
+        return reverse(temp);
+        
+    }
+    public ListNode reverse(ListNode head){
+        if(head == null) return null;
+        ListNode curr = head;
+        ListNode past = null;
+        while(curr != null){
+            // create and save future
+            ListNode future = curr.next;
+            curr.next = past;
+            past = curr;
+            curr = future;
         }
-        return dummy.next;
-
-        // tc = O(n) + O(n)
-        // sc = O(n) + O(n) dummy node
+        return past;
     }
 }
