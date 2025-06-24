1cad9c4e76d5c7981c206464de025a9c105db60a
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-06-24 23:55:20 +0530

    Time: 2 ms (83.41%) | Memory: 44.3 MB (96.83%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+    // so its of type 2 of recursion/
+    // type 2
+    List<List<Integer>> ans = new ArrayList<>();
+    dfs(arr,ans,0,0,new ArrayList<>(),target);
+    return ans; 
+    }
+    public void dfs(int[] arr,List<List<Integer>> ans, int sum, int index,ArrayList<Integer> list,int target){
+                    if(sum == target){
+                        ans.add(new ArrayList<>(list)); // deep copy
+                        return;
+                    }
+                    if(sum > target){
+                        return;
+                    }
+                    if(index == arr.length){
+                        return;
+                    }
+                    
+        // at each step i have choices and decisoions
+        // hat to multiple choice from index + 1 to n-1
+        for(int i = index;i<arr.length;i++){
+            // pick call remove
+            list.add(arr[i]); // not index
+            dfs(arr,ans,sum + arr[i],i ,list,target);
+            list.remove(list.size() - 1);
+        }
+    }
+}
