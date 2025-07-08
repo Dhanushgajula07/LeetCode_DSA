91fe8f4e2f17cc5ae0d5e0c58bb057f973bd802a
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-07-08 22:43:08 +0530

    Time: 5 ms (59.99%) | Memory: 44.9 MB (77.63%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -27,6 +27,37 @@
 // }
 
 
+// class Solution {
+//     public int minDistance(String s1, String s2) {
+//         // so here is my choice diagram 
+//         // if matches then no problem
+//         // else i can MIN(insert, delete, replace)
+//         // base case is also good
+//         int m = s1.length();
+//         int n =s2.length();
+//         Integer [][] dp = new Integer[m][n];
+//         return solve(m-1,n-1,s1,s2,dp);
+//     }
+//     public int solve(int i,int j,String s1, String s2,Integer [][] dp){
+//         if(i < 0) return j +1; // deletions
+//         if(j<0) return i+1; // insertions
+        
+//         if(dp[i][j] != null) return dp[i][j];
+
+//         if(s1.charAt(i) == s2.charAt(j)){
+//             // matched 
+//             return dp[i][j] = solve(i-1,j-1,s1,s2,dp);
+//         }
+//         else{
+//             return  dp[i][j] = 1 + Math.min(
+//                 solve(i,j-1,s1,s2,dp), Math.min(solve(i-1,j,s1,s2,dp),solve(i-1,j-1,s1,s2,dp))
+//             );
+//         }
+//     }
+// }
+
+
+
 class Solution {
     public int minDistance(String s1, String s2) {
         // so here is my choice diagram 
@@ -35,24 +66,24 @@ class Solution {
         // base case is also good
         int m = s1.length();
         int n =s2.length();
-        Integer [][] dp = new Integer[m][n];
-        return solve(m-1,n-1,s1,s2,dp);
-    }
-    public int solve(int i,int j,String s1, String s2,Integer [][] dp){
-        if(i < 0) return j +1; // deletions
-        if(j<0) return i+1; // insertions
-        
-        if(dp[i][j] != null) return dp[i][j];
+        int[][] dp = new int[m+1][n+1];
 
-        if(s1.charAt(i) == s2.charAt(j)){
+          // Base cases
+        for (int i = 0; i <= m; i++) dp[i][0] = i; // delete all characters from s1
+        for (int j = 0; j <= n; j++) dp[0][j] = j; // insert all characters to s1
+
+        for(int i =1;i<=m;i++){
+            for(int j =1;j<=n;j++){
+        if(s1.charAt(i-1) == s2.charAt(j-1)){
             // matched 
-            return dp[i][j] = solve(i-1,j-1,s1,s2,dp);
+             dp[i][j] = dp[i-1][j-1];
         }
         else{
-            return  dp[i][j] = 1 + Math.min(
-                solve(i,j-1,s1,s2,dp), Math.min(
-                    solve(i-1,j,s1,s2,dp),solve(i-1,j-1,s1,s2,dp))
-            );
+              dp[i][j] = 1 + Math.min(
+                dp[i][j-1], Math.min(dp[i-1][j],dp[i-1][j-1]));
         }
+            }
+        }
+            return dp[m][n];
     }
 }
