7262fbb31d81302fa1412d8b1f956c831529245b
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-07-08 22:03:10 +0530

    Time: 17 ms (85.71%) | Memory: 51.2 MB (55.02%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,3 +1,36 @@
+// class Solution {
+//     public int numDistinct(String s, String t) {
+//         // so its like i have choice of pick or no pick if the char mnatches
+//         // else i have no choice but to try thr next char
+//         // this is my choice diagram     
+//         int m = s.length();
+//         int n = t.length();
+//         Integer [][] dp = new Integer[m][n];
+//         return solve(m-1,n-1,s,t,dp);
+//     }
+//     public int solve(int i,int j,String s,String t,Integer[][] dp){
+//         if(j <0) return 1;
+//         if(i<0) return 0;
+
+//         // memo
+//         if(dp[i][j] != null){
+//             return dp[i][j];
+//         }
+//         if(s.charAt(i) == t.charAt(j)){
+//             // i have choice either take it or try for other occurance
+//             int pick = solve(i-1,j-1,s,t,dp);
+//             int nopick = solve(i-1,j,s,t,dp);
+//             return dp[i][j] = pick + nopick;  
+//         }
+//         else{
+//             return dp[i][j] = solve(i-1,j,s,t,dp);
+//         }
+
+//     }
+// }
+
+
+// tabulation
 class Solution {
     public int numDistinct(String s, String t) {
         // so its like i have choice of pick or no pick if the char mnatches
@@ -5,26 +38,31 @@ class Solution {
         // this is my choice diagram     
         int m = s.length();
         int n = t.length();
-        Integer [][] dp = new Integer[m][n];
-        return solve(m-1,n-1,s,t,dp);
-    }
-    public int solve(int i,int j,String s,String t,Integer[][] dp){
-        if(j <0) return 1;
-        if(i<0) return 0;
+        int [][] dp = new int[m+1][n+1];
 
-        // memo
-        if(dp[i][j] != null){
-            return dp[i][j];
+        // base case
+        // if (j<0) return 1
+        // mean when j string is "" it can be for sure formned by picking none
+
+        for(int j=0;j<m;j++){
+            dp[j][0] = 1;
         }
-        if(s.charAt(i) == t.charAt(j)){
+
+        // no of states =.no of looops
+        for(int i=1;i<=m;i++){
+            for(int j = 1;j<=n;j++){
+        if(s.charAt(i-1) == t.charAt(j-1)){
             // i have choice either take it or try for other occurance
-            int pick = solve(i-1,j-1,s,t,dp);
-            int nopick = solve(i-1,j,s,t,dp);
-            return dp[i][j] = pick + nopick;  
+            int pick = dp[i-1][j-1];
+            int nopick = dp[i-1][j];
+             dp[i][j] = pick + nopick;  
         }
         else{
-            return dp[i][j] = solve(i-1,j,s,t,dp);
+             dp[i][j] = dp[i-1][j];
         }
+            }}
 
+
+            return dp[m][n];
     }
 }
