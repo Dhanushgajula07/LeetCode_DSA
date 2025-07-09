ed0faa0b2cbc74a8ec3274fcdeb811b3331f49ef
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-07-09 22:40:25 +0530

    Time: 73 ms (25.01%) | Memory: 77.2 MB (19.06%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,39 +1,68 @@
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//              // so basically based on thgis their are many questions that can be solved so lets make it soo soo slow and do it ok..
+
+//              // so this is my choice diagram
+//              // if(arr[i] > arr[prev]){i have choiuce of picking or nop 
+//              // else no choice i shpuld skip
+//              // so i should track the prev index 
+//              // and previuously we solved stock rlated problems that also we use from  0 to n-1
+//              // here also we will go from 0 to n-1
+
+//              int n = nums.length;
+//              // it can have -va values so i will use integer
+//              Integer[][] dp = new Integer[n][n+1];
+//              // so now we will shift prev
+//             return solve(0,-1,nums,dp);
+//     }
+//     public int solve (int i,int prev,int[] arr,Integer[][] dp ){
+//         // base case
+//         int n = arr.length;
+//         if(i == n){
+//                 return 0;
+//         }
+
+//         if(dp[i][prev + 1] != null){
+//             return dp[i][prev + 1];
+//         }
+//         // choice diagram
+//         if(prev == -1 || arr[i] > arr[prev]){
+//             // i have chpice of pick or no
+//             int pick = 1 + solve(i + 1,i,arr,dp);
+//             int nopick = solve(i+1,prev,arr,dp);
+//             return dp[i][prev + 1] = Math.max(pick,nopick);
+//         }
+//         else{
+//             return dp[i][prev + 1] = solve(i+1,prev,arr,dp);
+//         }
+//     }
+// }
+
+
+
+// so we will optimise it
+
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-             // so basically based on thgis their are many questions that can be solved so lets make it soo soo slow and do it ok..
-
-             // so this is my choice diagram
-             // if(arr[i] > arr[prev]){i have choiuce of picking or nop 
-             // else no choice i shpuld skip
-             // so i should track the prev index 
-             // and previuously we solved stock rlated problems that also we use from  0 to n-1
-             // here also we will go from 0 to n-1
-
-             int n = nums.length;
+    public int lengthOfLIS(int[] arr) {
+             int n = arr.length;
              // it can have -va values so i will use integer
-             Integer[][] dp = new Integer[n][n+1];
-             // so now we will shift prev
-            return solve(0,-1,nums,dp);
-    }
-    public int solve (int i,int prev,int[] arr,Integer[][] dp ){
-        // base case
-        int n = arr.length;
-        if(i == n){
-                return 0;
-        }
-
-        if(dp[i][prev + 1] != null){
-            return dp[i][prev + 1];
-        }
-        // choice diagram
+             int[][] dp = new int[n+1][n+2];
+            // base is all 0
+        // i will come from right to left
+        for(int i = n-1;i>=0;i--){
+            for(int prev = i-1;prev>=-1;prev--){
+                        // choice diagram
         if(prev == -1 || arr[i] > arr[prev]){
             // i have chpice of pick or no
-            int pick = 1 + solve(i + 1,i,arr,dp);
-            int nopick = solve(i+1,prev,arr,dp);
-            return dp[i][prev + 1] = Math.max(pick,nopick);
+            int pick = 1 + dp[i + 1][i+1];
+            int nopick = dp[i+1][prev +1];
+                dp[i][prev + 1] = Math.max(pick,nopick);
         }
         else{
-            return dp[i][prev + 1] = solve(i+1,prev,arr,dp);
+            dp[i][prev + 1] = dp[i+1][prev + 1];
         }
+            }
+        }
+        return dp[0][0];
     }
 }
