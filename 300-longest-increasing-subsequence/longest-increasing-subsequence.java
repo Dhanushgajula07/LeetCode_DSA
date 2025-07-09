1717ee5ec779385af8e1707864b1a5548223e945
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-07-09 23:20:43 +0530

    Time: 4 ms (93.48%) | Memory: 44.5 MB (47.11%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -42,27 +42,98 @@
 
 // so we will optimise it
 
+// class Solution {
+//     public int lengthOfLIS(int[] arr) {
+//              int n = arr.length;
+//              // it can have -va values so i will use integer
+//              int[][] dp = new int[n+1][n+2];
+//             // base is all 0
+//         // i will come from right to left
+//         for(int i = n-1;i>=0;i--){
+//             for(int prev = i-1;prev>=-1;prev--){
+//                         // choice diagram
+//         if(prev == -1 || arr[i] > arr[prev]){
+//             // i have chpice of pick or no
+//             int pick = 1 + dp[i + 1][i+1];
+//             int nopick = dp[i+1][prev +1];
+//                 dp[i][prev + 1] = Math.max(pick,nopick);
+//         }
+//         else{
+//             dp[i][prev + 1] = dp[i+1][prev + 1];
+//         }
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// }
+
+
+// using method for print and more
+
+// class Solution {
+//     public int lengthOfLIS(int[] arr) {
+//              int n = arr.length;
+//              int[] dp = new int[n];
+
+//             // fill with 1 because singel is also valid
+//             Arrays.fill(dp,1);
+
+//              for(int i = 0;i<n;i++){
+//                 for(int j = 0;j<i;j++){
+//                     // check if arr[j] can be part
+//                     // it basically say longest subsequence that ends at index i
+//                     if(arr[i] > arr[j] && 1+dp[j] > dp[i]){
+//                         // can be part
+//                         dp[i] = 1+dp[j];
+//                     }
+//                 }
+//              }
+
+//              int max = Arrays.stream(dp).max().getAsInt();
+
+
+//              return max;
+//     }
+// }
+
+
+
+
+// now lets use Binary search 
+// we use lower bound and replace that number
+// offcource fomed string is not coorect but lenght is correct
+
+import java.util.*;
+
 class Solution {
     public int lengthOfLIS(int[] arr) {
-             int n = arr.length;
-             // it can have -va values so i will use integer
-             int[][] dp = new int[n+1][n+2];
-            // base is all 0
-        // i will come from right to left
-        for(int i = n-1;i>=0;i--){
-            for(int prev = i-1;prev>=-1;prev--){
-                        // choice diagram
-        if(prev == -1 || arr[i] > arr[prev]){
-            // i have chpice of pick or no
-            int pick = 1 + dp[i + 1][i+1];
-            int nopick = dp[i+1][prev +1];
-                dp[i][prev + 1] = Math.max(pick,nopick);
-        }
-        else{
-            dp[i][prev + 1] = dp[i+1][prev + 1];
-        }
+        List<Integer> lis = new ArrayList<>();
+
+        for (int num : arr) {
+            int idx = lowerBound(lis, num);
+            if (idx == lis.size()) {
+                lis.add(num);  // extend LIS
+            } else {
+                lis.set(idx, num);  // replace with smaller value
             }
         }
-        return dp[0][0];
+
+        return lis.size();
+    }
+
+    // Binary search for the first index in list where element >= target
+    private int lowerBound(List<Integer> list, int target) {
+        int low = 0, high = list.size();
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (list.get(mid) >= target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
     }
 }
