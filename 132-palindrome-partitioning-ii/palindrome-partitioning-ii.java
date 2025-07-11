22c9948e33ffb7c4ad0b17bd6053503c0772f400
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-07-11 12:03:30 +0530

    Time: 2500 ms (29.53%) | Memory: 41.4 MB (88.39%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minCut(String s) {
+      // so its like    Palindrome Partitioning I we solved in recursion and backtracking
+      // it was all the palindromes
+      // here minimum cuts that's the diffrence
+      // we do for loop based recursion ok...!
+
+      // lets memo
+      int n = s.length();
+      int[] memo = new int[n];
+      Arrays.fill(memo,-1);
+      return solve(0,s,memo) - 1;
+    }
+    public int solve(int i,String s,int[] memo){
+        int n = s.length();
+        if(i == n){
+            return 0;
+        }
+
+        if(memo[i] != -1){
+            return memo[i];
+        }
+        int min = Integer.MAX_VALUE;
+        // for loop based
+        for(int j = i;j<n;j++){
+            if(isPalindrome(i,j,s)){
+                int count = 1 + solve(j + 1,s,memo);    
+            min = Math.min(min,count);
+            }
+        }
+        return memo[i] = min;
+    }
+    public boolean isPalindrome(int i,int j,String s){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j))
+            return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
