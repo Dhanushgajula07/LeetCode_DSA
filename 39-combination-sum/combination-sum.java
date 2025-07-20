657dda948bdc1c7c54fa4a0124b5f935de14d399
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-07-20 22:13:33 +0530

    Time: 2 ms (82.86%) | Memory: 44.2 MB (98.74%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,33 +1,35 @@
 class Solution {
-    // mistakes 
-    // 1. deep copy
-    // 2. store i and alsi add i not index
     public List<List<Integer>> combinationSum(int[] arr, int target) {
-    // so its of type 2 of recursion/
-    // type 2
-    List<List<Integer>> ans = new ArrayList<>();
-    dfs(arr,ans,0,0,new ArrayList<>(),target);
-    return ans; 
+    // so its a backtracking pattern
+    // which means i can pick and do stuff and revert back
+
+        // pass by refernce 
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(0,arr,ans,new ArrayList<>(),target);
+        return ans;
     }
-    public void dfs(int[] arr,List<List<Integer>> ans, int sum, int index,ArrayList<Integer> list,int target){
-                    if(sum == target){
-                        ans.add(new ArrayList<>(list)); // deep copy
-                        return;
-                    }
-                    if(sum > target){
-                        return;
-                    }
-                    if(index == arr.length){
-                        return;
-                    }
-                    
-        // at each step i have choices and decisoions
-        // hat to multiple choice from index + 1 to n-1
-        for(int i = index;i<arr.length;i++){
-            // pick call remove
-            list.add(arr[i]); // not index
-            dfs(arr,ans,sum + arr[i],i ,list,target);
-            list.remove(list.size() - 1);
+    public void dfs(int idx, int[] arr, List<List<Integer>> ans,ArrayList<Integer> current,int target){
+        // IBH
+        // base case
+
+        if(target ==0){
+            ans.add(new ArrayList<>(current));
+        }
+        if(target <0){
+            return;
+        }
+        if(idx == arr.length){
+            return;
+        }
+
+        // now thr hoice diagram
+        // at every index i have a choce of pick or no pick
+        for(int i = idx;i<arr.length;i++){
+            // pick
+            current.add(arr[i]);
+            dfs(i,arr,ans,current,target - arr[i]);
+            // backtrack
+            current.remove(current.size() - 1);
         }
     }
 }
