50a102c126db0a73abffe20655d5b22ab35c7c74
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-07-24 11:53:53 +0530

    Time: 28 ms (97.56%) | Memory: 56.6 MB (74.78%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,29 +1,51 @@
+// class Solution {
+//     public int maxFrequency(int[] nums, int k) {
+//         int n = nums.length;
+//         int l = 0;
+//         int ans = 0;
+//         long sum = 0;  // Total sum of the current window
+
+//         Arrays.sort(nums); // Step 1: sort the array
+
+//         for (int r = 0; r < n; r++) {
+//             sum += nums[r]; // Add current number to the window sum
+
+//             // Check if window is valid
+//             long tohave = (long)(r - l + 1) * nums[r]; // total required sum to make all elements nums[r]
+//             long got = sum + k; // what we can afford using k increments
+
+//             while (tohave > got) {
+//                 sum -= nums[l]; // remove leftmost element
+//                 l++;
+//                 tohave = (long)(r - l + 1) * nums[r]; // update for new window
+//                 got = sum + k;
+//             }
+
+//             ans = Math.max(ans, r - l + 1); // update answer
+//         }
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        int n = nums.length;
-        int l = 0;
-        int ans = 0;
-        long sum = 0;  // Total sum of the current window
+        
+        int l=0,maxFreq=0;
+        long total=0;
+        Arrays.sort(nums);
+        
+        for(int r=0;r<nums.length;r++){
+            total += nums[r];
 
-        Arrays.sort(nums); // Step 1: sort the array
-
-        for (int r = 0; r < n; r++) {
-            sum += nums[r]; // Add current number to the window sum
-
-            // Check if window is valid
-            long tohave = (long)(r - l + 1) * nums[r]; // total required sum to make all elements nums[r]
-            long got = sum + k; // what we can afford using k increments
-
-            while (tohave > got) {
-                sum -= nums[l]; // remove leftmost element
+            while((long)nums[r] * (r-l+1) - total > k){
+                total -= nums[l];
                 l++;
-                tohave = (long)(r - l + 1) * nums[r]; // update for new window
-                got = sum + k;
             }
 
-            ans = Math.max(ans, r - l + 1); // update answer
+            maxFreq = Math.max(maxFreq,r-l+1);
         }
-
-        return ans;
+        return maxFreq;        
     }
 }
