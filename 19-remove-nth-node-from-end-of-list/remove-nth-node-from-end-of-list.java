977d6b74c80ffdbbc23472d87888e0d4fd78a1d2
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-09-20 00:48:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (33.56%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -8,38 +8,62 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         // so basically move i can use 2 pointets
+//         // 1. lets use that counting trick
+//         int total = count(head);
+//         int cut = total - n;
+//         ListNode node = head;      
+//         if(n== 1 && total == 1){
+//             return null;
+//         }
+
+
+//         // this is more important Edge Case
+//         if(n == total){
+//             return head.next;
+//         }
+//         while(node != null){
+//             cut--;
+//             if(cut ==0 && node.next != null){
+//                 node.next = node.next.next;
+//                 break;
+//             }
+//             node = node.next;
+//         } 
+//         return head;
+//     }
+//     public int count(ListNode head){
+//         int count = 0;
+//         if(head == null){
+//             return 0;
+//         }
+//         while(head != null){
+//             count++;
+//             head = head.next;
+//         }
+//         return count;
+//     }
+// }
+
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // so basically move i can use 2 pointets
-        // 1. lets use that counting trick
-        int total = count(head);
-        int cut = total - n;
-        ListNode node = head;      
-        if(n== 1 && total == 1){
-            return null;
-        }
-        if(n == total){
-            return head.next;
-        }
-        while(node != null){
-            cut--;
-            if(cut ==0 && node.next != null){
-                node.next = node.next.next;
-                break;
-            }
-            node = node.next;
-        } 
-        return head;
+    // 2.approch we will use 2 pointers 
+    ListNode fast = head;
+    for(int i = 0;i<n;i++){
+        fast = fast.next;
     }
-    public int count(ListNode head){
-        int count = 0;
-        if(head == null){
-            return 0;
-        }
-        while(head != null){
-            count++;
-            head = head.next;
-        }
-        return count;
+    ListNode slow = head;
+    if(fast == null) return head.next;
+    while(fast.next != null){
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+
+    return head;
     }
 }
