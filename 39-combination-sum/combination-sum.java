b533c2381ded0091c140614c0a583531d31d5ada
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-09-27 09:24:03 +0530

    Time: 2 ms (84.14%) | Memory: 44.9 MB (31.19%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,35 +1,32 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] arr, int target) {
-    // so its a backtracking pattern
-    // which means i can pick and do stuff and revert back
-
-        // pass by refernce 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        dfs(0,arr,ans,new ArrayList<>(),target);
+        generate(candidates, target, 0, 0, new ArrayList<>(), ans);
         return ans;
     }
-    public void dfs(int idx, int[] arr, List<List<Integer>> ans,ArrayList<Integer> current,int target){
-        // IBH
+
+    public void generate(int[] arr, int target, int sum, int idx, ArrayList<Integer> current, List<List<Integer>> ans) {
         // base case
-
-        if(target ==0){
-            ans.add(new ArrayList<>(current));
-        }
-        if(target <0){
-            return;
-        }
-        if(idx == arr.length){
+        if (sum >= target) {
+            if (sum == target)
+                ans.add(new ArrayList<>(current));
             return;
         }
 
-        // now thr hoice diagram
-        // at every index i have a choce of pick or no pick
-        for(int i = idx;i<arr.length;i++){
-            // pick
-            current.add(arr[i]);
-            dfs(i,arr,ans,current,target - arr[i]);
-            // backtrack
-            current.remove(current.size() - 1);
+        // its all the combinations
+        // its a pick or no pick based + can repeat same number multiple times
+
+        if (sum > target || idx >= arr.length) {
+            return;
         }
+        // current.add(arr[idx]);
+        // generate(arr,target,sum += arr[idx],idx,current,ans);
+        // current.remove(current.size()-1);
+        // generate(arr,target,sum -= arr[idx],idx+ 1,current,ans);
+        current.add(arr[idx]);
+        generate(arr, target, sum + arr[idx], idx, current, ans); // pick
+        current.remove(current.size() - 1);
+        generate(arr, target, sum, idx + 1, current, ans); // skip
+
     }
 }
