b81d1801346ff95e12357d775ed0859347b7b7b5
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-10-10 20:20:29 +0530

    Time: 2 ms (97.93%) | Memory: 63.4 MB (15.17%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -19,25 +19,47 @@
 
 
 
+// class Solution {
+//     public int maximumEnergy(int[] energy, int k) {
+//         int n = energy.length;
+//         Integer[] dp = new Integer[n];  // memo array
+//         int maxEnergy = Integer.MIN_VALUE;
+
+//         for (int i = 0; i < n; i++) {
+//             int val = dfs(energy, i, k, dp);
+//             maxEnergy = Math.max(maxEnergy, val);
+//         }
+
+//         return maxEnergy;
+//     }
+
+//     private int dfs(int[] arr, int i, int k, Integer[] dp) {
+//         if (i >= arr.length) return 0;
+//         if (dp[i] != null) return dp[i];
+
+//         dp[i] = arr[i] + dfs(arr, i + k, k, dp);
+//         return dp[i];
+//     }
+// }
+
+// tabulation
+
 class Solution {
     public int maximumEnergy(int[] energy, int k) {
         int n = energy.length;
-        Integer[] dp = new Integer[n];  // memo array
+        int[] dp = new int[n];
         int maxEnergy = Integer.MIN_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            int val = dfs(energy, i, k, dp);
-            maxEnergy = Math.max(maxEnergy, val);
+        // Fill from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + k < n)
+                dp[i] = energy[i] + dp[i + k];
+            else
+                dp[i] = energy[i];
+
+            maxEnergy = Math.max(maxEnergy, dp[i]);
         }
 
         return maxEnergy;
-    }
-
-    private int dfs(int[] arr, int i, int k, Integer[] dp) {
-        if (i >= arr.length) return 0;
-        if (dp[i] != null) return dp[i];
-
-        dp[i] = arr[i] + dfs(arr, i + k, k, dp);
-        return dp[i];
     }
 }
