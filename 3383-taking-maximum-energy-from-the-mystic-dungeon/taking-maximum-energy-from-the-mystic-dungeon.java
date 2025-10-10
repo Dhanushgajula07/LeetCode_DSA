6be6030cf1f18e80a3e1fe5fad25d139ece2607e
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-10-10 20:43:42 +0530

    Time: 3 ms (60.69%) | Memory: 63.6 MB (12.41%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -38,31 +38,58 @@ class Solution {
     //     return arr[idx] + solve(arr, idx + k,k);
     // }
 
-    public int maximumEnergy(int[] energy, int k) {
-        // now lets convery recursion to memoisation
-        // and remember its left to right
-        int max = Integer.MIN_VALUE;
-        int [] dp = new int[energy.length];
-        Arrays.fill(dp,-1);
+//     public int maximumEnergy(int[] energy, int k) {
+//         // now lets convery recursion to memoisation
+//         // and remember its left to right
+//         int max = Integer.MIN_VALUE;
+//         int [] dp = new int[energy.length];
+//         Arrays.fill(dp,-1);
 
-        for (int i = 0; i < energy.length; i++) {
-            int sum = solve(energy, i, k,dp);
-            max = Math.max(max, sum);
+//         for (int i = 0; i < energy.length; i++) {
+//             int sum = solve(energy, i, k,dp);
+//             max = Math.max(max, sum);
+//         }
+//         return max;
+//     }
+
+//     public int solve(int[] arr, int idx, int k,int[] dp) {
+//         // base case
+//         if (idx >= arr.length) {
+//             return 0;
+//         }
+//         // base case 2
+//         if(dp[idx] != -1){
+//             return dp[idx];
+//         }
+
+//         dp[idx] = arr[idx] + solve(arr,idx + k,k,dp);
+//         return dp[ idx];
+//     }
+
+
+public int maximumEnergy(int[] energy, int k) {
+// Now lest solve it using Tabulation
+// and remember now ist from right to left
+    int[] dp = new int[energy.length];
+    // base case to initilisation
+    // nothing to initialise
+
+    // no of state changes to no of loops
+    for(int i = energy.length - 1;i>=0;i--){
+        // now function call to dp
+        // returbn dp[0]
+        if(i + k < energy.length){
+        dp[i] = energy[i] + dp[i + k];
         }
-        return max;
+        else{
+            dp[i] = energy[i];
+        }
     }
-
-    public int solve(int[] arr, int idx, int k,int[] dp) {
-        // base case
-        if (idx >= arr.length) {
-            return 0;
-        }
-        // base case 2
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-
-        dp[idx] = arr[idx] + solve(arr,idx + k,k,dp);
-        return dp[ idx];
+    // now answer is max of all
+    int max = Integer.MIN_VALUE;
+    for(int val : dp){
+        max = Math.max(max,val);
     }
+    return max;
+}
 }
