6272daebfd88d5d7c94de602625da723aed7f7e1
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-11-26 10:08:05 +0530

    Time: 3 ms (10.43%) | Memory: 50.1 MB (5.21%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,116 +1,89 @@
 // class Solution {
 //     public int maxProfit(int[] prices) {
-//         // i did come up with choice diagram
-//         // if buy flag then i have choice to buy or no buy
-//         // else nobuy flag then i have choice to sell or no sell
+//     // i have a choice of buy and sell at any given day 
+//     // and also i must buy before sell
+//     // so i shulc chek if i aklready buied or no to sell
+//     // you can buy and sell multiple times
 
-//         // recursion
-//         // 1. i should represent everything in the form of index
-//         // 2. do everything or explore all the things that should be done 
-//         // 3. now retuen what question asked
-//         // 4. be sure with base cases
-//         int n = prices.length;
-//         int[][] dp = new int[n][2];
-//         for(int i =0;i<n;i++){
-//             for(int j= 0;j<2;j++){
-//             dp[i][j] = -1; 
-//             }
-//         } 
-//         return solve(0,prices,1,dp);
+//     // so all this will set into to recursion
+//     // lets write choice diagram
+//     // replrestnt everything in terms of index
+//     // already done
+
+//     // choice diagram
+//     // at evey index i have choice of either buy/ no-buy if already sold
+//     // if buyed then sell/ no-sell
+
+//     int n = prices.length;
+//     int flag_buy = 1;
+//     return solve(0,n,flag_buy,prices);
 //     }
-//     public int solve(int i,int[] prices,int buy,int[][] dp){
-//         int n = prices.length;
-//         if(i == n){
+//     private int solve(int idx,int n,int flag_buy,int[] prices){
+//         if(idx == n){
 //             return 0;
 //         }
-//         if(dp[i][buy] != -1) return dp[i][buy];
-//         int profit;
-//         if(buy == 1){
-//             // i can buy or no
-//             int buyed = -prices[i] + solve(i+1,prices,0,dp);
-//             int skip = 0 + solve(i+1,prices,1,dp);
-//             profit =  Math.max(buyed,skip); 
+//         int profit = 0;
+//         if(flag_buy == 1){
+//             // i can buy or no buy
+//             int buy = -prices[idx] + solve(idx + 1,n,0,prices);
+//             // no buy
+//             int no_buy = 0 + solve(idx+1,n,1,prices);
+//             profit = Math.max(buy,no_buy);
 //         }
 //         else{
-//             int sell = prices[i] + solve(i+1,prices,1,dp);
-//             int skip = 0 + solve(i+1,prices,0,dp);
-//             profit =   Math.max(sell,skip);
+//             int sell = prices[idx] + solve(idx +1,n,1,prices);
+//             int no_sell = 0 + solve(idx + 1,n,0,prices);
+//             profit = Math.max(sell,no_sell);
 //         }
-//         return dp[i][buy] = profit;
+//         return profit;
 //     }
 // }
 
 
-// // tabulation
-// class Solution {
-//     public int maxProfit(int[] prices) {
-//         // i did come up with choice diagram
-//         // if buy flag then i have choice to buy or no buy
-//         // else nobuy flag then i have choice to sell or no sell
 
-//         // recursion
-//         // 1. i should represent everything in the form of index
-//         // 2. do everything or explore all the things that should be done 
-//         // 3. now retuen what question asked
-//         // 4. be sure with base cases
-//         int n = prices.length;
-//         int[][] dp = new int[n+1][2];
-//         int buy = 1;
-//         // so its stock so tabulation should ne from n to 0
-//         for(int i = n-1;i>=0;i--){
-//             for(int j = 0;j<=1;j++){
-//         if(j == 1){
-//             // i can buy or no
-//             int buyed = -prices[i] + dp[i+1][0];
-//             int skip = 0 + dp[i+1][1];
-//             dp[i][j] =  Math.max(buyed,skip); 
-//         }
-//         else{
-//             int sell = prices[i] + dp[i+1][1];
-//             int skip = 0 + dp[i+1][0];
-//             dp[i][j] =   Math.max(sell,skip);
-//         }
-//             }
-//         }
-//         return dp[0][1];
-//     }
-// }
-
-
-// just 2 1d arrays
-
-class Solution {
+class Solution { // memo
     public int maxProfit(int[] prices) {
-        // i did come up with choice diagram
-        // if buy flag then i have choice to buy or no buy
-        // else nobuy flag then i have choice to sell or no sell
+    // i have a choice of buy and sell at any given day 
+    // and also i must buy before sell
+    // so i shulc chek if i aklready buied or no to sell
+    // you can buy and sell multiple times
 
-        // recursion
-        // 1. i should represent everything in the form of index
-        // 2. do everything or explore all the things that should be done 
-        // 3. now retuen what question asked
-        // 4. be sure with base cases
-        int n = prices.length;
-        int[] next = new int[2];
-        int buy = 1;
-        // so its stock so tabulation should ne from n to 0
-        for(int i = n-1;i>=0;i--){ // it sayd how many times i should loop 
-            int[] curr = new int[2];
-            for(int j = 0;j<=1;j++){
-        if(j == 1){
-            // i can buy or no
-            int buyed = -prices[i] + next[0];
-            int skip = 0 + next[1];
-            curr[j] =  Math.max(buyed,skip); 
+    // so all this will set into to recursion
+    // lets write choice diagram
+    // replrestnt everything in terms of index
+    // already done
+
+    // choice diagram
+    // at evey index i have choice of either buy/ no-buy if already sold
+    // if buyed then sell/ no-sell
+
+    int n = prices.length;
+    int flag_buy = 1;
+    // variables are index and buy or no
+    int[][] dp = new int[n + 1][2];
+    for(int[] arr : dp){
+        Arrays.fill(arr,-1);
+    }
+    return solve(0,n,flag_buy,prices,dp);
+    }
+    private int solve(int idx,int n,int flag_buy,int[] prices,int[][] dp){
+        if(idx == n){
+            return 0;
+        }
+        int profit = 0;
+        if(dp[idx][flag_buy] != -1) return dp[idx][flag_buy];
+        if(flag_buy == 1){
+            // i can buy or no buy
+            int buy = -prices[idx] + solve(idx + 1,n,0,prices,dp);
+            // no buy
+            int no_buy = 0 + solve(idx+1,n,1,prices,dp);
+            profit = Math.max(buy,no_buy);
         }
         else{
-            int sell = prices[i] + next[1];
-            int skip = 0 + next[0]; // i have only 0 and 1 index 
-            curr[j] =   Math.max(sell,skip);
+            int sell = prices[idx] + solve(idx +1,n,1,prices,dp);
+            int no_sell = 0 + solve(idx + 1,n,0,prices,dp);
+            profit = Math.max(sell,no_sell);
         }
-            }
-            next = curr;
-        }
-        return next[1];
+        return dp[idx][flag_buy] = profit;
     }
 }
