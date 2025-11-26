f1ab5ac49cc0e18cd793be746a6be5f521cdcc95
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-11-26 10:27:32 +0530

    Time: 18 ms (12.97%) | Memory: 73.8 MB (12.42%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -15,19 +15,33 @@ class Solution {
         // missing++;
         // }
 
-        // bettert
+        // better
         // use boolean array
-        boolean[] seen = new boolean[nums.length];
+        // boolean[] seen = new boolean[nums.length];
+        // int n = nums.length;
+        // for(int num : nums){
+        //     if (num > 0 && num <= n) 
+        //     seen[num - 1] = true;
+        // }
+        // for(int i = 0;i<n;i++){
+        //     if(seen[i] == false){
+        //         return i+1;
+        //     }
+        // }
+        // return n+1;
+
+
+        // optimal 1
+        // use sort
+
         int n = nums.length;
-        for(int num : nums){
-            if (num > 0 && num <= n) 
-            seen[num - 1] = true;
-        }
-        for(int i = 0;i<n;i++){
-            if(seen[i] == false){
-                return i+1;
+        Arrays.sort(nums);
+        int missing = 1;
+        for (int num : nums) {
+            if (num > 0 && missing == num) {
+                missing++;
             }
         }
-        return n+1;
+        return missing;
     }
 }
