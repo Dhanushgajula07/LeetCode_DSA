8e7de7ac68900d1794258246b3b372ad06177424
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2025-12-15 22:16:42 +0530

    Time: 2 ms (100.00%) | Memory: 84.1 MB (54.07%) - LeetSync

---
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long ans = 1; // first day is always a descent period
+        long streak = 1;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i - 1] - prices[i] == 1) {
+                streak++;
+            } else {
+                streak = 1;
+            }
+            ans += streak;
+        }
+        return ans;
+    }
+}
