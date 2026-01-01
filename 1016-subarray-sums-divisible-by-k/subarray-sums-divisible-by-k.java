6707179e2d99e3af63461b182fbd74f4918fbf2f
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2026-01-01 12:33:40 +0530

    Time: 25 ms (46.91%) | Memory: 51.7 MB (41.10%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        // so its about devision, when is it divisble if reminder is 0
+        // when is it not if its not 0
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1); // -> this will guie wrong ans becuase its like having 0 mean divisble 
+        int sum = 0;
+        int count = 0;
+        for(int num : nums){
+            sum += num;
+            int rem = sum % k;
+            if(rem < 0) rem = (rem + k) % k;
+            if(map.containsKey(rem)){
+                // that is sum % k == 0
+                count += map.get(rem);
+            }
+            map.put(rem,map.getOrDefault(rem,0) + 1);
+        }
+        return count;
+    }
+}
