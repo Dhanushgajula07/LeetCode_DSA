9305351be9ededed114d0631a8f993804a9aa223
Dhanushgajula07 <123740160+Dhanushgajula07@users.noreply.github.com>
2026-01-12 23:12:59 +0530

    Time: 1 ms (30.15%) | Memory: 46.8 MB (22.79%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -12,43 +12,27 @@
  *         this.right = right;
  *     }
  * }
- */class Solution {
-        public int maxPathSum(TreeNode root) {
-        // main catch is that node can appear atmopst once..
-        // and also path might ot might not pass thorugh the root
-        // ans its any non empty path
+ */
 
-        // its like if the answer from left or right is going  -ve then we just return that node
-        // its of type any node to node
-        // and we also want the max global var or int[] max
 
+class Solution {
+    public int maxPathSum(TreeNode root) {
         int[] max = new int[1];
         max[0] = Integer.MIN_VALUE;
-        solve(root,max);
+        dfs(root,max);
         return max[0];
+    }
+    public int dfs(TreeNode root,int[] max){
+        // base
+        if(root == null){
+            return 0;
         }
-        public int solve(TreeNode node,int[] max){
-            // so this the templet
-            // int return,
-            // max[] actual answer if any to any
-            // solve method with IBH---> 
-            // now get the temp answer and also check can you be he bigger answer
+        // inductions
+        int lm = Math.max(0,dfs(root.left,max));
+        int rm = Math.max(0,dfs(root.right,max));
 
-            // base case
-            if(node == null){
-                return 0;
-            }
+        max[0] = Math.max(max[0],lm + rm + root.val);
 
-            // left and rigth
-            int left = Math.max(0,solve(node.left,max));
-
-            int right = Math.max(0,solve(node.right,max));
-
-
-            // it does not fallow the tree rules
-            int temp = node.val + left + right;
-            max[0] = Math.max(max[0],temp);
-
-            return node.val + Math.max(left,right);
-        }
- }
+        return root.val + Math.max(lm,rm);
+    }
+}
